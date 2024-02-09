a8fcf7f8cd57a132db97256c5afb613c35a391bf
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2024-02-09 23:52:47 +0100

    Block scoped and eslint obeyed

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    //    const newTask = true; // eslint-disable-line error - no unused vars
+    //    const newTask2 = false; // eslint-disable-line error - no unused vars
+  }
+
+  return [task, task2];
+}
