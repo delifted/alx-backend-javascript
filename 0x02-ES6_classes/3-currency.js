3779f9a9c7258b482a2f3acdcb93820ea80c42b1
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2024-03-14 07:25:14 +0100

    Currency building using ES6 Class

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,4 +1,31 @@
-/* eslint-disable no-underscore-dangle */
+// /* eslint-disable no-underscore-dangle */
+
+// export default class Currency {
+//   constructor(code, name) {
+//     this.code = code;
+//     this.name = name;
+//   }
+
+//   get name() {
+//     return this._name;
+//   }
+
+//   set name(value) {
+//     this._name = value;
+//   }
+
+//   get code() {
+//     return this._code;
+//   }
+
+//   set code(value) {
+//     this._code = value;
+//   }
+
+//   displayFullCurrency() {
+//     return (`${this.name} + (${this.code})`);
+//   }
+// }
 
 export default class Currency {
   constructor(code, name) {
@@ -6,23 +33,27 @@ export default class Currency {
     this.name = name;
   }
 
-  get name() {
-    return this._name;
-  }
-
-  set name(value) {
-    this._name = value;
-  }
-
   get code() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._code;
   }
 
   set code(value) {
+    // eslint-disable-next-line no-underscore-dangle
     this._code = value;
   }
 
+  get name() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._name;
+  }
+
+  set name(value) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._name = value;
+  }
+
   displayFullCurrency() {
-    return (`${this.name} + (${this.code})`);
+    return `${this.name} (${this.code})`;
   }
 }
