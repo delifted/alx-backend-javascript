96f9490c672673d01b69632bee2304c5cbaa07e5
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2024-05-05 19:51:58 +0100

    Standard input on the terminal via JS

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,30 @@
+// process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+// process.stdin.on('readable', () => {
+//     const name = process.stdin.read();
+
+//     if (name) {
+//         process.stdout.write(`Your name is: ${name}`);
+//     }
+// });
+
+// process.stdin.on('end', () => {
+//     process.stdout.write('This important software is now closing\n');
+// });
+
+// 1-stdin.js
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-    const name = process.stdin.read();
+process.stdin.setEncoding('utf8');
 
-    if (name) {
-        process.stdout.write(`Your name is: ${name}`);
-    }
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}\n`);
+  }
 });
 
 process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
