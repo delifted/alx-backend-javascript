68a7ab64a949fb1080d5b3d3a574517e22735a82
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2024-05-05 15:15:06 +0100

    Using spies to execute wrapped function and logging information about function called

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const sendPaymentRequestToApi =  require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function () {
+    it('should use Utils.calculateNumber with correct arguments', function () {
+        const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+        sendPaymentRequestToApi(100, 20);
+
+        expect(calculateNumberSpy.calledOnce).to.be.true;
+        expect(calculateNumberSpy.calledWithExactly('SUM', 100, 20)).to.be.true;
+
+        calculateNumberSpy.restore();
+    })
+})
